d4c4b622fad0feb04cc6600b7a720c7c467cc3df
ukiuki-engineer <yuki.hyodo0201@gmail.com>
2024-03-10 02:45:19 +0900

    section4: 27

---
+++ 04_react_basic/src/070_component_nest/start/components/Child.js
@@ -0,0 +1,13 @@
+import { List } from "./List";
+import "./Child.css";
+
+const Child = () => {
+  return (
+    <div className="component">
+      <h3>Hello Component</h3>
+      <List />
+    </div>
+  );
+};
+
+export default Child;
