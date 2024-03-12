52d95301ac3a222205e468c8a039a35d25a67eb1
ukiuki-engineer <yuki.hyodo0201@gmail.com>
2024-03-13 02:46:20 +0900

    section4-35: 110_props_children

--- 04_react_basic/src/110_props_children/start/Example.js
+++ 04_react_basic/src/110_props_children/start/Example.js
@@ -2,14 +2,17 @@ import Profile from "./components/Profile";
 import Container from "./components/Container";
 
 const profile = [
-  { name: "Takashi", age: 19, country: "Japan", color: "green" },
-  { name: "Jane", age: 28, country: "UK", color: "blue" },
+  { name: "Takashi", age: 19, country: "Japan" },
+  { name: "Jane", age: 28, country: "UK", color: "red" },
 ];
 
 const Example = () => {
   return (
     <div>
-      <Container title="Childrenとは？" />
+      <Container title="順番通り渡す">
+        <Profile key={profile[0].name} {...profile[0]} />
+        <Profile key={profile[1].name} {...profile[1]} />
+      </Container>
     </div>
   );
 };
--- 04_react_basic/src/110_props_children/start/components/Container.js
+++ 04_react_basic/src/110_props_children/start/components/Container.js
@@ -1,9 +1,12 @@
 import "./Container.css";
 
-const Container = ({ title }) => {
+const Container = ({ title, children, first, second, color }) => {
   return (
-    <div className="container">
+    <div className={"container " + color}>
       <h3>{title}</h3>
+      <div>{children}</div>
+      <div>{first}</div>
+      <div>{second}</div>
     </div>
   );
 };
