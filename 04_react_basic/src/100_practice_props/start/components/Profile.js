b197f5c83713708af8c0adbf2fa933495c397bd5
ukiuki-engineer <yuki.hyodo0201@gmail.com>
2024-03-13 01:46:41 +0900

    section4-35: 100_practice_props

--- 04_react_basic/src/100_practice_props/start/components/Profile.js
+++ 04_react_basic/src/100_practice_props/start/components/Profile.js
@@ -1,12 +1,16 @@
 import "./Profile.css";
 
-const Profile = (props) => {
+const Profile = ({ name = "John Doe", age = "??", country = "Japan" }) => {
   return (
     <div className="profile">
-
+      {/* name */}
+      <h3>Name: {name}</h3>
+      {/* age */}
+      <p>Age: {age}</p>
+      {/* from */}
+      <p>From: {country}</p>
     </div>
   );
 };
-
 
 export default Profile;
