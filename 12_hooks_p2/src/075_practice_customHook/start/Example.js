33f2cf7e3300991d3ca2039d87e96d7d34b92a35
ukiuki-engineer <yuki.hyodo0201@gmail.com>
2024-05-15 00:29:40 +0900

    custom hook

--- 12_hooks_p2/src/075_practice_customHook/start/Example.js
+++ 12_hooks_p2/src/075_practice_customHook/start/Example.js
@@ -1,13 +1,11 @@
+import { useCount } from "./hooks";
+
 const Example = () => {
+  const { count, countUp } = useCount();
   return (
     <>
-      <h3>練習問題</h3>
-      <p>
-        記述を変更し、完成コードと同じ状態になるようにしてください。
-        startフォルダの中にhooks.jsというファイルを作成しその中でuseCountというカスタムフックを作成してください。
-      </p>
-      <div>Counts: {}</div>
-      <button onClick={() => {}}>Count Up!</button>
+      <div>Counts: {count}</div>
+      <button onClick={countUp}>Count Up!</button>
     </>
   );
 };
